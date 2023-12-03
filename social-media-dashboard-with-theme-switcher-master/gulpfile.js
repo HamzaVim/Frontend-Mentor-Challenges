590e22c6c9d2fa40d5fc0b675af717307db979ae
HamzaSx <HamzaSxH@gmail.com>
2023-12-03 14:48:09 +0300

    Set up gulp workflow

---
+++ social-media-dashboard-with-theme-switcher-master/gulpfile.js
@@ -0,0 +1,57 @@
+// Initialize modul
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const babel = require("gulp-babel");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync").create();
+
+// Sass task
+function scssTask() {
+  return src("app/scss/style.scss", { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest("dist", { sourcemaps: "." }));
+}
+
+// Js task
+function jsTask() {
+  return src("app/js/script.js", { sourcemaps: true })
+    .pipe(babel({ presets: ["@babel/preset-env"] }))
+    .pipe(terser())
+    .pipe(dest("dist", { sourcemaps: "." }));
+}
+
+// Browsersync
+function browserSyncServer(cb) {
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+    notify: {
+      styles: {
+        top: "auto",
+        bottom: "0",
+      },
+    },
+  });
+  cb();
+}
+function browserSyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch task
+function watchTask() {
+  watch("*.html", browserSyncReload);
+  watch(
+    ["app/scss/**/*.scss", "app/js/**/*.js"],
+    series(scssTask, jsTask, browserSyncReload)
+  );
+}
+
+// Default gulp Task
+exports.default = series(scssTask, jsTask, browserSyncServer, watchTask);
